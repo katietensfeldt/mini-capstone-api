f2d1bdf0bcf1966c190a35a05e4c08d411a90431
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-05 12:20:13 -0700

    Adds product instances to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Monstera Plant", price: 20, image_url: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1205923822-612x612-1597694031.jpg", description: "Medium sized monstera plant. Great for adding fresh air to the home.")
+Product.create(name: "Fidget Spinner", price: 7, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fidget_spinner_red%2C_cropped.jpg/1200px-Fidget_spinner_red%2C_cropped.jpg", description: "Small toy designed to relieve stress and improve focus.")
