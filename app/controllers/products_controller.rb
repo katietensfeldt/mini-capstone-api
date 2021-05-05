05cf9a092bc50a6deda13fca8473572c35a40f82
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-05 12:33:36 -0700

    Adds routes for each individual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,20 @@
 class ProductsController < ApplicationController
+  #Displays all products on the page
   def all_products
     products = Product.all
     render json: products.as_json
   end
+
+  #Individual method for each product
+  def monstera_plant
+    product = Product.find_by(name: "Monstera Plant")
+    render json: product.as_json
+  end
+
+  def fidget_spinner
+    product = Product.find_by(name: "Fidget Spinner")
+    render json: product.as_json
+  end
+
+  #The process of adding routes for each thing is so inconvenient! if we had a much larger shop, it would be nearly impossible!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_products"
+  get "/monstera_plant" => "products#monstera_plant"
+  get "/fidget_spinner" => "products#fidget_spinner"
 end
