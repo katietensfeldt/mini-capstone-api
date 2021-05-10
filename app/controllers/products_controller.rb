e9300ed8920d9e86b05da9466ac2178173aee8b0
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-10 14:08:54 -0700

    Adds destroy and update actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,35 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product = Product.find(params[:id])
     render json: product.as_json
+  end
+  
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.description = params[:description] || product.description
+    product.image_url = params[:image_url] || product.image_url
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Product successfully destroyed"}
   end
 end
--- script.rb
+++ script.rb
@@ -13,12 +13,13 @@ puts "Here are the products we offer:"
 products.each do |product|
   puts "==========================================="
   puts
+  puts "ID: #{product["id"]}"
   puts product["name"]
   puts "$#{product["price"]}"
   puts
 end
 
-puts "If you would like to view more about a product, please enter a number, 1-3"
+puts "If you would like to view more about a product, please enter product id"
 id = gets.chomp
 
 response = HTTP.get("http://localhost:3000/products/#{id}")
@@ -32,3 +33,36 @@ puts "$#{product["price"]}"
 puts product["description"]
 puts product["image_url"]
 puts
+
+puts "Please add an item to our store!"
+puts
+puts "What is the name of the product?"
+name = gets.chomp
+puts "What is the price?"
+price = gets.chomp.to_i
+puts "Describe the product"
+description = gets.chomp
+puts "What is the image url?"
+image_url = gets.chomp
+
+response = HTTP.post("http://localhost:3000/products", :params => {:name => name, :price => price, :description => description, :image_url => image_url})
+
+response = HTTP.get("http://localhost:3000/products")
+
+products = response.parse
+
+puts
+puts "Here is the updated list of products we offer:"
+products.each do |product|
+  puts "==========================================="
+  puts
+  puts "ID: #{product["id"]}"
+  puts product["name"]
+  puts "$#{product["price"]}"
+  puts
+end
+
+puts "If you would like to delete an item, please enter the id of the product you would like to delete."
+id = gets.chomp
+
+response = HTTP.delete("http://localhost:3000/products/#{id}")
