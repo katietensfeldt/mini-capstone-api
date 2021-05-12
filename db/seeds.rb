000c29f5ff89b40a0913b8f4929f10f9fb83657b
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-12 12:31:40 -0700

    Runs seed dump on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# Product.create(name: "Monstera Plant", price: 20, image_url: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1205923822-612x612-1597694031.jpg", description: "Medium sized monstera plant. Great for adding fresh air to the home.")
-# Product.create(name: "Fidget Spinner", price: 7, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fidget_spinner_red%2C_cropped.jpg/1200px-Fidget_spinner_red%2C_cropped.jpg", description: "Small toy designed to relieve stress and improve focus.")
-Product.create(name: "Women's Hiking Boots", price: 60, image_url: "https://media.cntraveler.com/photos/5f99c33ff5bb028212972c2a/master/w_2100,h_1500,c_limit/Columbia-WomensHikingBoots-2020.jpg", description: "These boots were made for walking... on a hiking trail!")
+Product.create!([
+  {name: "Monstera Plant", price: "20.0", image_url: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1205923822-612x612-1597694031.jpg", description: "Medium sized monstera plant. Great for adding fresh air to the home.", inventory: 5},
+  {name: "Fidget Toy", price: "7.0", image_url: "https://c.shld.net/rpx/i/s/pi/mp/10160405/prod_9189258932?src=http%3A%2F%2Flykartstore.site%2Flykartimage%2FimageB%2FALVB07T4SV1Z4.jpg&d=597ae5c1f31c76fe1e875613b2671d0e187d3a63&hei=333&wid=333&op_sharpen=1", description: "Small toy designed to relieve stress and improve focus.", inventory: 20},
+  {name: "Women's Hiking Boots", price: "75.0", image_url: "https://media.cntraveler.com/photos/5f99c33ff5bb028212972c2a/master/w_2100,h_1500,c_limit/Columbia-WomensHikingBoots-2020.jpg", description: "These boots were made for walking... on a hiking trail!", inventory: 7},
+  {name: "Pineapple Scented Candle", price: "10.0", image_url: "https://i.pinimg.com/564x/86/c6/84/86c684608dffa3e6460abd86d4a833d1.jpg", description: "Bring the smell of hospitality to your home.", inventory: 13},
+  {name: "Tinkerbell Funko Pop", price: "10.0", image_url: "https://cdn11.bigcommerce.com/s-ccbay/images/stencil/2048x2048/products/30780/126260/api5umm2o__57639.1592329287.jpg?c=2", description: "Small figurine portraying the beloved character, Tinkerbell", inventory: 5},
+  {name: "DnD Dice", price: "25.5", image_url: "https://i.etsystatic.com/13261405/r/il/69f594/3031780627/il_794xN.3031780627_op1t.jpg", description: "Beautiful green tea inspired dnd dice set.", inventory: 18},
+  {name: "Tazo Matcha Mate Grapefruit", price: "5.0", image_url: "https://www.simpleskincare.com/sk-eu/content/dam/brands/tazo/canada/1971685-794522002221.png", description: "1 box of grapefruit matcha green tea. The box contains 18 individual tea bags.", inventory: 21},
+  {name: "Hydration Backpack", price: "80.0", image_url: "https://www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw6d5df9b2/images/hi-res/48440_GYPG.jpg?sw=1300&sh=1300&sfrm=png&q=85&bgcolor=f6f6f6", description: "Backpack meant to keep you hydrated on long hikes.", inventory: 4},
+  {name: "Yoga Mat", price: "20.0", image_url: "https://static.contrado.com/DesignTemplates/products/65059_paradise-house-tropical-floral-blue-yoga-mat_0.jpeg?cache=6", description: "Floral yoga mat to help stretch and find peace of mind.", inventory: 10},
+  {name: "Succulent Garden", price: "45.5", image_url: "https://www.vivaterra.com/medias/sys_master/images/images/hc5/h86/10923817369630/succulent-garden-in-root-bowl.jpg", description: "Bring a small lovely desert-scape closer. This lovely planter is filled with some of the most beautiful desert plants.", inventory: 3}
+])
