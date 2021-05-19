991116ccad5c7b2ded3d0cd2ef219a079a15ec1e
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-19 12:19:03 -0700

    Adds supplier model and controller

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :image_url, :inventory, :is_discounted?, :tax, :total
+  attributes :id, :name, :price, :description, :image_url, :inventory, :is_discounted?, :tax, :total, :supplier
 end
