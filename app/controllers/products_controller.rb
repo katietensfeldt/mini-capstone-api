7a8fc03acd198f9cdb40f2a7eef1e48e1d44c02b
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-24 13:58:02 -0700

    Adds params search for categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,21 +3,27 @@ class ProductsController < ApplicationController
 
   def index
     products = Product.all
-    if params[:search_term]
-      products = products.where("name iLIKE ?", "%#{params[:search_term]}%")
+
+    if params[:category]
+      category = Category.find_by("name iLIKE?", "%#{params[:category]}%")
+      products = category.products
     end
-    if params[:sort] == "price"
-      if params[:sort_order] == "desc"
-        products = products.order(price: :desc)
-      else
-        products = products.order(:price)
-      end
-    else
-      products = products.order(:id)
-    end
-    if params[:discount] == "true"
-      products = products.where("price < 10")
-    end
+
+    # if params[:search_term]
+    #   products = products.where("name iLIKE ?", "%#{params[:search_term]}%")
+    # end
+    # if params[:sort] == "price"
+    #   if params[:sort_order] == "desc"
+    #     products = products.order(price: :desc)
+    #   else
+    #     products = products.order(:price)
+    #   end
+    # else
+    #   products = products.order(:id)
+    # end
+    # if params[:discount] == "true"
+    #   products = products.where("price < 10")
+    # end
 
     render json: products
    
